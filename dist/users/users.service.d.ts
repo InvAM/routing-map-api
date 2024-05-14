e64ee9bea72724aef12de250313d221012b79296
Adrian Alvarez <adriantramarria@gmail.com>
2024-05-14 00:56:21 -0500

    add services for users

--- dist/users/users.service.d.ts
+++ dist/users/users.service.d.ts
@@ -1,2 +1,8 @@
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 export declare class UsersService {
+    private userRepository;
+    constructor(userRepository: Repository<User>);
+    createUser(user: CreateUserDto): Promise<User>;
 }
